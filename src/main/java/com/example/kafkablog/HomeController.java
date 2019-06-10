8341d264f2407465c2208073cf76cc9e61fc1b7d
Rogerio Chaves <rogeriocfj@gmail.com>
2019-06-10 23:23:11 +0200

    First commit, basic spring template and kafka dependencies

---
+++ src/main/java/com/example/kafkablog/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.kafkablog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/"})
+    public String home(Model model) {
+        return "home";
+    }
+}
