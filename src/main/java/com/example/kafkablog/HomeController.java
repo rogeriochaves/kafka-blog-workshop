84c7b88068369788e211fe9cfdf728d2b79ac2a3
Rogerio Chaves <rogeriocfj@gmail.com>
2019-06-14 00:11:07 +0200

    step 1 - adding post model and save endpoint

--- src/main/java/com/example/kafkablog/HomeController.java
+++ src/main/java/com/example/kafkablog/HomeController.java
@@ -4,10 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @Controller
 public class HomeController {
     @GetMapping({"/"})
     public String home(Model model) {
+        model.addAttribute("post", new Post());
+
         return "home";
+    }
+
+    @PostMapping({"/posts/save"})
+    public String save(@ModelAttribute Post post) {
+        UUID uuid = UUID.randomUUID();
+        post.setId(uuid.toString());
+        System.out.print(post);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/example/kafkablog/Post.java
@@ -0,0 +1,22 @@
+package com.example.kafkablog;
+
+public class Post {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String id;
+    private String title;
+}
